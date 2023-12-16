f14ae01dfe9306d77960ebda167de4216512ff8a
Melike Kutlu <kutlu21@itu.edu.tr>
2023-12-16 22:40:50 +0300

    comment line added

--- 13-21-2023_sky_hw/falling_algo.py
+++ 13-21-2023_sky_hw/falling_algo.py
@@ -15,8 +15,8 @@ from camera_class import camera_class
 import datetime
 import numpy as np
 
-def falling_algo(vehicle,current_location,target_location):
-    
+def falling_algo(vehicle, current_location, target_location):
+    # Constants for falling algorithm
     rho = 1.225
     g = 9.80665
     m = 0.370
@@ -24,6 +24,7 @@ def falling_algo(vehicle,current_location,target_location):
     radius = 0.044
     A = math.pi * radius**2
     
+    # Retrieve altitude and speeds from the vehicle
     h = vehicle.location.global_relative_frame.alt
     if h < 0:
         h = 0
@@ -31,22 +32,29 @@ def falling_algo(vehicle,current_location,target_location):
     airSpeed = vehicle.airspeed
     windSpeed = groundSpeed - airSpeed
     
+    # Handle edge cases to prevent division by zero
     if groundSpeed == 0:
         groundSpeed = 0.00001
     if airSpeed == 0:
         airSpeed = 0.00001
     if windSpeed == 0:
-        windSpeed =0.00001
+        windSpeed = 0.00001
 
+    # Calculate parameters for the falling algorithm
     P = rho * Cd * A / (2 * m)
     k = P * m
     t = math.sqrt(abs(m/(g*k))) * math.acosh(math.exp(h*k/m))
     c1 = -1 / airSpeed
     c2 = -(-math.log(airSpeed) + windSpeed / airSpeed) / P
+    
+    # Calculate the range based on falling algorithm
     range = (math.log(P * t - c1) - c1 * windSpeed + P * t * windSpeed) / P + c2
 
-    distance = geopy.distance.GeodesicDistance((target_location.lat,target_location.lon),
-                                            (current_location.lat,
-                                            current_location.lon)).meters
+    # Calculate the distance between current and target locations
+    distance = geopy.distance.GeodesicDistance(
+        (target_location.lat, target_location.lon),
+        (current_location.lat, current_location.lon)
+    ).meters
     
-    return (distance-range, distance, range)
+    # Return tuple containing calculated values
+    return (distance - range, distance, range)
