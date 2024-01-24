b69024efaf31c861075a91307bbcd020eb16417a
AhmetKerem33 <145498951+AhmetKerem33@users.noreply.github.com>
2024-01-24 15:55:54 +0300

    Update main_mission_toward_duba.py

--- rasp_folder/teknofest_2023_flight_code/main_mission_toward_duba.py
+++ rasp_folder/teknofest_2023_flight_code/main_mission_toward_duba.py
@@ -68,7 +68,8 @@ camera_bot = camera_class(video,fourcc,out)
 
 
 print("Trying to connect to the vehicle...")
-vehicle = connect('/dev/ttyACM0', baud=57600, wait_ready=True)
+# vehicle = connect('/dev/ttyACM0', baud=57600, wait_ready=True)
+vehicle = connect("127.0.0.1:14550", wait_ready=True)
 print("Connected to the vehicle.")
 
 cmds=vehicle.commands
